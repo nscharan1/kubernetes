014c1c1d5eec881dfea1b6d3a967b787bf0d5221
sulakshan2503 <59904601+sulakshan2503@users.noreply.github.com>
2020-07-28 02:13:12 -0700

    Add files via upload

---
+++ bootstrap-master.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Initialize Kubernetes
+echo "[TASK 1] Initialize Kubernetes Cluster"
+sudo su - root -c "kubeadm init --kubernetes-version=1.15.3 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null"
+
+# Copy Kube admin config
+echo "[TASK 2] Copy kube admin config to centos user .kube directory"
+sudo su - root -c "mkdir /home/centos/.kube"
+sudo su - root -c "cp /etc/kubernetes/admin.conf /home/centos/.kube/config"
+sudo su - root -c "chown -R centos:centos /home/centos/.kube"
+
+# Deploy calico network
+echo "[TASK 3] Deploy calico network"
+sudo su - root -c "yum install wget -y"
+cd /home/centos && wget https://raw.githubusercontent.com/PrasadTelasula/k8s_calico/master/calico.yaml
+kubectl create -f /home/centos/calico.yaml
+
+# Generate Cluster join command
+echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
+sudo su - root -c "kubeadm token create --print-join-command > /joincluster.sh"
+sudo su - root -c "cat /joincluster.sh"
